e32e27c259789fcfcc6568021576ff9f892fbd53
ArtVis69 <azrifaris305@gmail.com>
2025-11-12 08:03:27 +0700

    First Update

---
+++ README.md
@@ -0,0 +1,29 @@
+### To-Do-List by ArtVis
+----------------------------------------------------------------------------------------------------
+
+## Purpose
+
+> This to-do list focused to train my **JS**.
+> You also can use this as your personal to-do list.
+
+----------------------------------------------------------------------------------------------------
+
+## Feature
+
+- Save to-do list with localstorage.
+- Delete to-do List.
+- Mark to-do List if "done".
+- Edit to-do List.
+
+----------------------------------------------------------------------------------------------------
+
+## Languange
+
+- **HTML** (Markup)
+- **CSS** (Style)
+- **JS**
+
+----------------------------------------------------------------------------------------------------
+## Note
+
+> if i has something wrong or feedback you can comment in my repository.
---
+++ script.js
@@ -0,0 +1,87 @@
+const taskinput = document.getElementById('task-input');
+const addtaskbutton = document.getElementById('add-task-button');
+const tasklist = document.getElementById('task-list');
+let taskcondition = false;
+
+//localstorage function
+const task = JSON.parse(localStorage.getItem("task")) || [];
+
+//RenderList function
+function renderList() {
+    tasklist.innerHTML = "";
+    task.forEach((taskitem, index) => {
+        const li = document.createElement("li");
+        li.innerHTML = `
+            <span>${taskitem}</span>
+            <button class="del" onclick = "deltask(${index})"><i class="fa-solid fa-xmark"></i></button>
+            <button class="done" onclick = "donetask(${index})"><i class="fa-solid fa-check"></i></button>
+            <button class="edit" onclick = "edittask(${index})"><i class="fa-solid fa-pen-to-square"></i></button>
+        `;
+        tasklist.appendChild(li);
+
+    });
+};
+
+//edit task function
+function edittask(index){
+    const newTask = prompt("Edit your task:", task[index])
+    if(newTask !== null && newTask.trim() !== ""){  
+        if(newTask.length > 50){
+            alert("Task cannot exceed 50 characters!");
+            edittask(index);
+        } else {
+            task[index] = newTask.trim();
+            savetask();
+            renderList();
+        }      
+    } else {
+        alert("Task cannot be empty!");
+        edittask(index);
+    }
+}
+
+//Add task function
+function addtask(){
+    const taskvalue = taskinput.value.trim();
+    if(taskvalue === "" || null) return alert ("please add a task");
+
+    task.push(taskvalue);
+    taskinput.value = "";
+
+    savetask();
+    renderList();
+}
+//Done task function
+function donetask(index){
+    const current = task[index];
+
+    if (typeof current !== 'string') return;
+
+    if (current.endsWith(' ✔')) {
+        task[index] = current.slice(0, -2);
+    } else { 
+        task[index] = current + ' ✔';
+    }
+    savetask();
+    renderList();
+}
+//Delete task function
+function deltask(index){
+    task.splice(index, 1);
+    savetask();
+    renderList();
+}
+
+//Save task function
+function savetask(){
+    localStorage.setItem("task", JSON.stringify(task));
+}
+
+//Event listener
+addtaskbutton.addEventListener("click", addtask);
+
+taskinput.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") addtask ();
+})
+
+renderList();
